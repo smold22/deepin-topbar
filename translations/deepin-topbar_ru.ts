6a803e8f5a3a931b3bdb1ea6f06095d4130c5dbe
smold22 <vlasov5020@gmail.com>
2018-02-18 19:46:30 +0700

    Add files via upload
    Added localization for Russia

---
+++ translations/deepin-topbar_ru.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>dtb::datetime::DateTimePlugin</name>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="49"/>
+        <source>24 часа</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="53"/>
+        <source>12 часов</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="69"/>
+        <source>Формат времени</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="74"/>
+        <source>Параметры времени</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::AppstoreAction</name>
+    <message>
+        <location filename="../modules/indicator/View/appstoreaction.cpp" line="108"/>
+        <source>%n обновление(я)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::SystemLogo</name>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="67"/>
+        <source>О программе</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="79"/>
+        <source>Предпочтительные настройки</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="82"/>
+        <source>Сброс</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="83"/>
+        <source>Сон</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="84"/>
+        <source>Перезагрузка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="85"/>
+        <source>Выключение</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="86"/>
+        <source>Выход из системы %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WiredItem</name>
+    <message>
+        <location filename="../modules/network/item/wireditem.cpp" line="27"/>
+        <source>Открыть настройки сети...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WirelessItem</name>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="73"/>
+        <source>Присоединиться к другой сети...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="74"/>
+        <source>Открыть настройки сети...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="320"/>
+        <source>Пароль, необходимый для подключения &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::notify::NotifyModel</name>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="180"/>
+        <source>Вчерашний день</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="183"/>
+        <source>Позавчерашний день</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../modules/notify/notifymodel.cpp" line="189"/>
+        <source>%n день(и) ago</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>dtb::power::PowerWidget</name>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="132"/>
+        <source>Источник энергии:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="132"/>
+        <source>Батарея</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="132"/>
+        <source>Постоянный ток</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="133"/>
+        <source>Показать процент</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="134"/>
+        <source>Открыть настройки энергосбережения</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundApplet</name>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="41"/>
+        <source>Устройство</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="61"/>
+        <source>Приложение</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundItem</name>
+    <message>
+        <location filename="../modules/sound/sounditem.cpp" line="47"/>
+        <source>Дополнительные настройки</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::systemtray::SystemTrayPlugin</name>
+    <message>
+        <location filename="../modules/system-tray/systemtrayplugin.cpp" line="23"/>
+        <source>Системный лоток</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::wallpaper::WallpaperSettings</name>
+    <message>
+        <location filename="../modules/wallpaper/wallpapersettings.cpp" line="39"/>
+        <source>Добавить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpapersettings.cpp" line="40"/>
+        <source>Удалить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpapersettings.cpp" line="41"/>
+        <source>Очистить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpapersettings.cpp" line="42"/>
+        <source>Применить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpapersettings.cpp" line="67"/>
+        <source>Добавить путь</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::wallpaper::WallpaperWidget</name>
+    <message>
+        <location filename="../modules/wallpaper/wallpaperwidget.cpp" line="70"/>
+        <source>DreamScene</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpaperwidget.cpp" line="79"/>
+        <source>Перейти к следующим обоям</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpaperwidget.cpp" line="80"/>
+        <source>Режим просмотра</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpaperwidget.cpp" line="81"/>
+        <source>Об DreamSense</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../modules/wallpaper/wallpaperwidget.cpp" line="82"/>
+        <source>Персональные настройки...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
